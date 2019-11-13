100645d8c7f5a5ca485c33c61f6059f3d8d60f33
Yomaru <mlkyeung@connect.ust.hk>
2019-11-13 19:06:36 +0800

    safety commit

--- src/main/java/sample/util.java
+++ src/main/java/sample/util.java
@@ -15,31 +15,7 @@ import tower.TowerInformation;
 class util {
 
 	
-	static void showTowerRangeAtFirstTime(Label target) {
-		if(GreenBoxes.targetHasTower(target) != true) 
-			return ; 
-		// TODO Auto-generated method stub
-		//if the tower is basic tower or ice tower 
-		switch(target.getId()) {
-		case "Basic Tower" : 
-		case "Ice Tower" :
-			//show range
-			Circle circle = new Circle () ; 
-			circle.setCenterX(MyController.GRID_WIDTH * ((double)GreenBoxes.targetV(target) + 0.5));
-			circle.setCenterY(MyController.GRID_WIDTH * ((double)GreenBoxes.targetH(target) + 0.5));
-			circle.setRadius(GreenBoxes.targetGetTower(target).getInfo().shooting_range);
-			circle.setStyle("-fx-border-color: rgba(0,0,0,0.3); -fx-border-width: 3");
-			break ; 
-		case "Catapult" : 
-			break ; 
-		case "Laser Tower" : 
-			break ; 
-			default : 
-				Alert alert = new Alert(AlertType.ERROR , "no such kind of tower") ; 
-				alert.showAndWait() ; 
-		}
-		
-	}
+	
 
 	static boolean moveMonsters(List<Monster> monsters) {
 		// TODO Auto-generated method stub
