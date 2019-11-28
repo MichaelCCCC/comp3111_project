c354606a34147ef2ea5dd380267a2adab8e8cf9c
Michael <michaelcheung1000@hotmail.com>
2019-11-29 00:48:59 +0800

    monster javadoc

--- src/main/java/monster/Fox.java
+++ src/main/java/monster/Fox.java
@@ -1,11 +1,30 @@
 package monster;
 
 import javafx.scene.control.Label;
-
+/**
+ * 
+ *	This is class for Fox 
+ * @author Michael
+ */
 public class Fox extends Monster {
+	/**
+	 * The {@link DEFAULT_FOX_HP} is the default HP for fox
+	 */
 	public static final int DEFAULT_FOX_HP = 5;
+	/**
+	 * The {@link DEFAULT_FOX_SPEED} is the default speed for fox
+	 */
 	public static final int DEFAULT_FOX_SPEED = 40;
+	/**
+	 * The {@link NAME} is the name of fox
+	 */
 	public static final String NAME = "Fox" ; 
+	
+	/**
+	 * This is the constructor of fox 
+	 * @param label the label to display fox
+	 * @param num_frame the number of frame of the game
+	 */
 	public Fox(Label label, int num_frame) {
 		super(NAME,DEFAULT_FOX_HP,DEFAULT_FOX_SPEED, label, num_frame);
 	}
--- src/main/java/monster/Unicorn.java
+++ src/main/java/monster/Unicorn.java
@@ -1,12 +1,30 @@
 package monster;
 
 import javafx.scene.control.Label;
-
+/**
+ * This is a class for Unicorn
+ * @author Michael
+ *
+ */
 public class Unicorn extends Monster {
+	/**
+	 * The {@link DEFAULT_UNICORN_HP} is the default HP of unicorn
+	 */
 	public static final int DEFAULT_UNICORN_HP = 20;
+	/**
+	 * The {@link DEFAULT_UNICRON_SPEED} is the default speed of unicorn
+	 */
 	public static final int DEFAULT_UNICRON_SPEED = 20;
-	public static String NAME = "UNICORN" ; 
+	/**
+	 * The {@link NAME} is the name of unicorn
+	 */
+	public static final String NAME = "UNICORN" ; 
 	
+	/**
+	 * This is the constructor of unicorn
+	 * @param label the label to display unicorn
+	 * @param num_frame the number of frame of the game
+	 */
 	public Unicorn(Label label, int num_frame) {
 		super(NAME, DEFAULT_UNICORN_HP,DEFAULT_UNICRON_SPEED, label, num_frame);
 	}
